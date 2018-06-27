0706cf091b6f0f99ac796200e8cfbfd068e41598
cryptosam212 <38875914+cryptosam212@users.noreply.github.com>
2018-06-27 16:04:08 +0700

    Update install_cubex.sh

--- install_cubex.sh
+++ install_cubex.sh
@@ -222,9 +222,9 @@ DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
 apt install -y software-properties-common >/dev/null 2>&1
 apt-get install unzip nano -y >/dev/null 2>&1
-echo -e "${PURPLE}Adding bitcoin PPA repository"
+echo -e "${PURPLE}Adding bitcoin PPA repository${NC}"
 apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
+echo -e "${YELLOW}Installing required packages, it may take some time to finish.${NC}"
 apt-get update >/dev/null 2>&1
 apt-get install libzmq3-dev -y >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
