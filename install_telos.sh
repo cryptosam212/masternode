80dee6310abd82adebfc6b990db38d7fd5a91d71
cryptosam212 <38875914+cryptosam212@users.noreply.github.com>
2018-08-28 23:49:54 +0700

    Update install_telos.sh

--- install_telos.sh
+++ install_telos.sh
@@ -114,7 +114,7 @@ rpcallowip=127.0.0.1
 listen=1
 server=1
 daemon=1
-
+staking=0
 port=$COIN_PORT
 EOF
 }
@@ -151,17 +151,7 @@ externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
 
 #ADDNODES
-addnode=149.28.37.27
-addnode=185.230.162.50
-addnode=45.76.226.137
-addnode=95.216.166.215
-addnode=176.223.128.58
-addnode=80.211.10.114
-addnode=213.183.51.247
-addnode=94.177.188.190
-addnode=80.211.109.173
-addnode=27.64.228.115
-addnode=45.76.241.7
+
 EOF
 }
 
