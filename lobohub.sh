5fac1cd022793347d8ae7f5ce7019025b01655c3
cryptosam212 <38875914+cryptosam212@users.noreply.github.com>
2018-09-15 19:21:15 +0700

    Update lobohub.sh

--- lobohub.sh
+++ lobohub.sh
@@ -128,13 +128,13 @@ then
   sudo apt-get update
   sudo apt-get install -y zip unzip
    cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+ # sudo touch swap.img
+ # sudo chmod 600 swap.img
+ # sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+ # sudo mkswap /var/swap.img
+ # sudo swapon /var/swap.img
+ # sudo free
+ # sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
   cd
   
  if [ ! -f Linux.zip ]
